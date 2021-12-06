780cc401d6e937767a3bbe27ac335b4607767498
graceshotts15 <78330999+graceshotts15@users.noreply.github.com>
2021-12-05 22:13:01 -0600

    Add files via upload

---
+++ yahtzeeJS.js
@@ -0,0 +1,54 @@
+function Roll1() {
+    var rollResult = Math.floor(Math.random() * 6) + 1;
+    var pic = document.getElementById("1");
+     if (rollResult == 1) pic.setAttribute('src', '1.png');
+    else if (rollResult == 2) pic.setAttribute('src', '2.png');
+    else if (rollResult == 3) pic.setAttribute('src', '3.png');
+    else if (rollResult == 4) pic.setAttribute('src', '4.png');
+    else if (rollResult == 5) pic.setAttribute('src', '5.png');
+    else  pic.setAttribute('src', '6.png');
+}
+function Roll2() {
+    var rollResult = Math.floor(Math.random() * 6) + 1;
+    var pic = document.getElementById("2");
+     if (rollResult == 1) pic.setAttribute('src', '1.png');
+    else if (rollResult == 2) pic.setAttribute('src', '2.png');
+    else if (rollResult == 3) pic.setAttribute('src', '3.png');
+    else if (rollResult == 4) pic.setAttribute('src', '4.png');
+    else if (rollResult == 5) pic.setAttribute('src', '5.png');
+    else  pic.setAttribute('src', '6.png');
+}
+function Roll3() {
+    var rollResult = Math.floor(Math.random() * 6) + 1;
+    var pic = document.getElementById("3");
+     if (rollResult == 1) pic.setAttribute('src', '1.png');
+    else if (rollResult == 2) pic.setAttribute('src', '2.png');
+    else if (rollResult == 3) pic.setAttribute('src', '3.png');
+    else if (rollResult == 4) pic.setAttribute('src', '4.png');
+    else if (rollResult == 5) pic.setAttribute('src', '5.png');
+    else  pic.setAttribute('src', '6.png');
+}
+function Roll4() {
+    var rollResult = Math.floor(Math.random() * 6) + 1;
+    var pic = document.getElementById("4");
+     if (rollResult == 1) pic.setAttribute('src', '1.png');
+    else if (rollResult == 2) pic.setAttribute('src', '2.png');
+    else if (rollResult == 3) pic.setAttribute('src', '3.png');
+    else if (rollResult == 4) pic.setAttribute('src', '4.png');
+    else if (rollResult == 5) pic.setAttribute('src', '5.png');
+    else  pic.setAttribute('src', '6.png');
+}
+function Roll5() {
+    var rollResult = Math.floor(Math.random() * 6) + 1;
+    var pic = document.getElementById("5");
+     if (rollResult == 1) pic.setAttribute('src', '1.png');
+    else if (rollResult == 2) pic.setAttribute('src', '2.png');
+    else if (rollResult == 3) pic.setAttribute('src', '3.png');
+    else if (rollResult == 4) pic.setAttribute('src', '4.png');
+    else if (rollResult == 5) pic.setAttribute('src', '5.png');
+    else  pic.setAttribute('src', '6.png');
+    }
+
+function Dice1(){
+
+}
